8b2357914534cd8907fac30b685027551f822417
Binu Meledom <binu.meledom@aptusdatalabs.com>
2018-10-03 15:35:54 +0530

    binus changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
- apiUrl: 'http://192.168.1.18:8080'
+ apiUrl: 'http://192.168.1.9:8080'
   //apiUrl: 'http://localhost:8080'
  //apiUrl: 'http://182.76.155.250:80'
 };
